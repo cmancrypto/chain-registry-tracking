71d9633463670592a77337c9349d668d8138928f
Chris <chris.clarke@sor.com.au>
2024-05-28 13:41:12 +1000

    updated main.py - send email based on repo content update refactored repo_contents.py to make tidier

--- chain-registry-tracking/main.py
+++ chain-registry-tracking/main.py
@@ -1,14 +1,53 @@
-import pandas as pd
-
-import pull_requests
 import email_notifications
+import repo_contents
+from loguru import logger
 
-cosmos_repo_pulls = pull_requests.RepoPulls("cosmos", "chain-registry")
-#dymension_repo_pulls= pull_requests.RepoPulls(github_user="dymensionxyz",github_repo_name="chain-registry")
-[new_pulls,existing_pulls,updated_pulls]=cosmos_repo_pulls.main()
-#dymension_repo_pulls.main()
+class RepoContentUpdateNotification():
+    def __init__(self,
+                 gh_repo_owner: str,
+                 gh_repo_name: str,
+                 notification_receivers: list,
+                 content_filter : repo_contents.ContentFilter
+                 ):
+        """
 
-str_new_pulls=new_pulls.drop(columns=["html_url"]).to_string()
-str_updated_pulls=updated_pulls.to_string()
-email_body="\n".join([str_new_pulls,str_updated_pulls])
-email_notifications.main("cosmos-registry-updates",email_body,["cmancrypto@outlook.com"])
+        :param gh_repo_owner: gh username that owns the repo to check
+        :param gh_repo_name: gh repo  name - combined with owner makes {owner}/{repo_name} i.e. cosmos/chain-registry
+        :param notification_receivers: list of email addresses to receive notification
+        :param content_filter: filters of type repo_contents.ContentFilter - used to build the content query
+        """
+        self.gh_repo_owner = gh_repo_owner
+        self.gh_repo_name = gh_repo_name
+        self.notification_receivers=notification_receivers
+        self.content_filter = content_filter
+
+    def main(self):
+        repo_contents_instance=repo_contents.RepoContents(self.gh_repo_owner,
+                                                          self.gh_repo_name,
+                                                          content_filter=self.content_filter)
+        new_contents=repo_contents_instance.main()
+        count_new=new_contents[new_contents.columns[0]].count()
+        logger.info(f"Got {count_new} new {self.content_filter.return_file_content_type} in {self.gh_repo_owner}/{self.gh_repo_name} ")
+
+        if count_new > 0:
+            #start to build the email
+            message_text=f"""
+            Got {count_new} new {self.content_filter.return_file_content_type} 
+            in {self.gh_repo_owner}/{self.gh_repo_name}
+            at path{self.content_filter.path} : \n
+            {new_contents.to_string()}
+            """
+            message_subject=f"Got {count_new} new {self.content_filter.return_file_content_type} in {self.gh_repo_owner}/{self.gh_repo_name}"
+            email_notifications.main(message_subject, message_text, self.notification_receivers)
+
+if __name__=="__main__":
+    content_filters = repo_contents.ContentFilter(name="testnets",
+                            path="testnets",
+                            return_file_content_type="dir",
+                            recursively_access=False)
+    chain_reg_testnet = RepoContentUpdateNotification("cosmos",
+                                  "chain-registry",
+                                  ["cmancrpyto@outlook.com"],
+                                  content_filter=content_filters)
+    chain_reg_testnet.main()
+
--- chain-registry-tracking/repo_contents.py
+++ chain-registry-tracking/repo_contents.py
@@ -28,25 +28,25 @@ class ContentFilter:
         self.recursively_access=recursively_access
 
 class RepoContents(RegistryTrackingBase):
-    def __init__(self, github_user, github_repo_name, content_filters : ContentFilter):
+    def __init__(self, github_user, github_repo_name, content_filter : ContentFilter):
         super().__init__(github_user=github_user, github_repo_name=github_repo_name, class_type="repo-contents")
-        self.content_filters=content_filters
+        self.content_filter=content_filter
 
 
     def get_path_contents(self) -> list:
-        if self.content_filters.path == None: path=""
+        if self.content_filter.path == None: path= ""
         try:
-            contents = self.repo.get_contents(self.content_filters.path)
+            contents = self.repo.get_contents(self.content_filter.path)
         except github.UnknownObjectException as e:
             logger.error(f"No content found at path for {self.github_repo_name}:{e}")
             raise
         return_contents=[]
         while contents:
             file_content=contents.pop(0)
-            if file_content.type == self.content_filters.return_file_content_type:
-                logger.info(f"got {self.content_filters.return_file_content_type} : {file_content.path}" )
+            if file_content.type == self.content_filter.return_file_content_type:
+                logger.info(f"got {self.content_filter.return_file_content_type} : {file_content.path}")
                 return_contents.append(file_content)
-            if self.content_filters.recursively_access == True and file_content.type =="dir":
+            if self.content_filter.recursively_access == True and file_content.type == "dir":
                 contents.extend(self.repo.get_contents(file_content.path))
                 logger.info(f"adding dir {file_content.path} to search")
         return return_contents
@@ -59,25 +59,14 @@ class RepoContents(RegistryTrackingBase):
         return converted_contents
 
     def main(self)->pandas.DataFrame:
-        existing_df=self.fetch_df_from_csv(version=self.content_filters.name)
+        existing_df=self.fetch_df_from_csv(version=self.content_filter.name)
         contents=self.get_path_contents()
         contents_names=self.convert_contents_to_names(contents)
         df=pd.DataFrame({"contents_names" : contents_names})
-        self.write_df_to_csv(df,version=self.content_filters.name)
+        self.write_df_to_csv(df, version=self.content_filter.name)
         ## ~ negates the isin operator to get new df contents NOT IN existing df
         new_contents_df=df[~df.iloc[:,0].isin(existing_df.iloc[:,0])]
         return new_contents_df
-
-
-
-
-content_filters=ContentFilter(name="testnets",
-                              path="testnets",
-                              return_file_content_type="dir",
-                              recursively_access=False)
-
-repo_contents=RepoContents("cosmos","chain-registry", content_filters=content_filters)
-repo_contents.main()
 
 
 
